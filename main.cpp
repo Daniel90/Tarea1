80f7d54e963c3ae8cf36d7f6d25d64b183fbc8f2
Daniel Abrilot <daniel.abrilot@gmail.com>
2013-03-27 17:20:10 -0700

    Actualizacion 2

--- main.cpp
+++ main.cpp
@@ -3,50 +3,57 @@
 #include <time.h>
 using namespace std;
 
+void mostrarM(int M[][8],int jugada)
+{
+     for(int i=0;i<=7;i++)
+     {
+           for(int j=0;j<=7;j++)
+               cout<<M[i][j]<<"     ";     
+           cout<<endl<<endl<<endl; 
+     }                           
+     cout<<"Jugada Numero: "<<jugada<<endl;
+     system("pause");
+     system("CLS");   
+}
 
 void llenarM(int M[][8])
 {
      
      for(int i=0;i<=7;i++)
          for(int j=0;j<=7;j++)
-             M[i][j]=0;
-     int i=7;
-     int j=0;
+             M[i][j]=0;            //--> Se llena la matriz de 8x8 con ceros
+     int i=7,j=0,jugada=1;
      M[i][j]=1;
      int sum=1;
      srand(time(NULL));
-     while(sum!=60)
+     mostrarM(M,jugada);            
+     while(sum!=60)                                    
      {
-         long num=(rand()%8)+1;
+         long num=(rand()%8)+1;   //-->Se generará un número al azar y se le asignara con una de las posibles jugadas que puede realizar el caballo
          switch(num)
          {
               case 1:
                    i=i-1;
                    j=j+2;
-                   if(i<0||i>7||j<0||j>7||(i==2 && j==2)||(i==2 && j==5)||(i==5 && j==2)||(i==5 && j==5))
+                   if(i<0||i>7||j<0||j>7||(i==2 && j==2)||(i==2 && j==5)||(i==5 && j==2)||(i==5 && j==5))//Condiciones de borde del tablero y los 4 peones
                    {
-                       i=i+1;
+                       i=i+1;                   //inverso de la jugada
                        j=j-2;
                    }
                    else
                    {
-                       if(M[i][j]==0)
+                       if(M[i][j]==0)                        //Si el caballo no ha estado antes en esa posición
                        {
                             M[i][j]=1;
                             sum++;
-                            for(int u=0;u<=7;u++)
-                            {
-                                    for(int w=0;w<=7;w++)
-                                            cout<<M[u][w]<<" ";     
-                                    cout<<endl; 
-                            }
-                            cout<<"Jugada Numero: "<<sum<<endl;
-                            system("pause");
-                            system("CLS");
-                            
+                            jugada++;
+                            mostrarM(M,jugada);
                        }
-                       else
-                       {
+                       else                       
+                       { 
+                             M[i][j]++; 
+                             jugada++;
+                             mostrarM(M,jugada);                 
                        }
                    }
                    break;
@@ -64,18 +71,14 @@ void llenarM(int M[][8])
                        {
                             M[i][j]=1;
                             sum++;
-                            for(int u=0;u<=7;u++)
-                            {
-                                    for(int w=0;w<=7;w++)
-                                            cout<<M[u][w]<<" ";     
-                                    cout<<endl; 
-                            }
-                            cout<<"Jugada Numero: "<<sum<<endl;
-                            system("pause");
-                            system("CLS");
+                            jugada++;
+                            mostrarM(M,jugada);
                        }
                        else
                        {
+                           M[i][j]++; 
+                           jugada++;
+                           mostrarM(M,jugada);
                        }
                    }
                    break;
@@ -93,18 +96,14 @@ void llenarM(int M[][8])
                        {
                             M[i][j]=1;
                             sum++;
-                            for(int u=0;u<=7;u++)
-                            {
-                                    for(int w=0;w<=7;w++)
-                                            cout<<M[u][w]<<" ";     
-                                    cout<<endl; 
-                            }
-                            cout<<"Jugada Numero: "<<sum<<endl;
-                            system("pause");
-                            system("CLS");
+                            jugada++;
+                            mostrarM(M,jugada);
                        }
                        else
                        {
+                           M[i][j]++; 
+                           jugada++;
+                           mostrarM(M,jugada);
                        }
                    }
                    break;
@@ -122,18 +121,14 @@ void llenarM(int M[][8])
                        {
                             M[i][j]=1;
                             sum++;
-                            for(int u=0;u<=7;u++)
-                            {
-                                    for(int w=0;w<=7;w++)
-                                            cout<<M[u][w]<<" ";     
-                                    cout<<endl; 
-                            }
-                            cout<<"Jugada Numero: "<<sum<<endl;
-                            system("pause");
-                            system("CLS");
+                            jugada++;
+                            mostrarM(M,jugada);
                        }
                        else
                        {
+                           M[i][j]++; 
+                           jugada++;
+                           mostrarM(M,jugada);
                        }
                    }
                    break;
@@ -151,18 +146,14 @@ void llenarM(int M[][8])
                        {
                             M[i][j]=1;
                             sum++;
-                            for(int u=0;u<=7;u++)
-                            {
-                                    for(int w=0;w<=7;w++)
-                                            cout<<M[u][w]<<" ";     
-                                    cout<<endl; 
-                            }
-                            cout<<"Jugada Numero: "<<sum<<endl;
-                            system("pause");
-                            system("CLS");
+                            jugada++;
+                            mostrarM(M,jugada);
                        }
                        else
                        {
+                           M[i][j]++; 
+                           jugada++;
+                           mostrarM(M,jugada);
                        }
                    }
                    break;
@@ -180,18 +171,14 @@ void llenarM(int M[][8])
                        {
                             M[i][j]=1;
                             sum++;
-                            for(int u=0;u<=7;u++)
-                            {
-                                    for(int w=0;w<=7;w++)
-                                            cout<<M[u][w]<<" ";     
-                                    cout<<endl; 
-                            }
-                            cout<<"Jugada Numero: "<<sum<<endl;
-                            system("pause");
-                            system("CLS");
+                            jugada++;
+                            mostrarM(M,jugada);
                        }
                        else
                        {
+                           M[i][j]++;
+                           jugada++; 
+                           mostrarM(M,jugada);
                        }
                    }
                    break;
@@ -209,18 +196,14 @@ void llenarM(int M[][8])
                        {
                             M[i][j]=1;
                             sum++;
-                            for(int u=0;u<=7;u++)
-                            {
-                                    for(int w=0;w<=7;w++)
-                                            cout<<M[u][w]<<" ";     
-                                    cout<<endl; 
-                            }
-                            cout<<"Jugada Numero: "<<sum<<endl;
-                            system("pause");
-                            system("CLS");
+                            jugada++;
+                            mostrarM(M,jugada);
                        }
                        else
                        {
+                           M[i][j]++; 
+                           jugada++;
+                           mostrarM(M,jugada);
                        }
                    }
                    break;
@@ -238,29 +221,19 @@ void llenarM(int M[][8])
                        {
                             M[i][j]=1;
                             sum++;
-                            for(int u=0;u<=7;u++)
-                            {
-                                    for(int w=0;w<=7;w++)
-                                            cout<<M[u][w]<<" ";     
-                                    cout<<endl; 
-                            }
-                            cout<<"Jugada Numero: "<<sum<<endl;
-                            system("pause");
-                            system("CLS");
+                            jugada++;
+                            mostrarM(M,jugada);
                        }
                        else
                        {
+                           M[i][j]++;
+                           jugada++; 
+                           mostrarM(M,jugada);
                        }
                    }
                    break;
          }
      }
-          
-                                    
-     
-     
-     cout<<sum;          
-
 }
 
 
